40858c600d645d606ff48a368d3519dad093ad4a
doanja <doanja@plu.edu>
2021-08-17 16:02:13 -0700

    updated styles for todo

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -8,14 +8,13 @@ interface TodoProps {
 
 const Todo = ({ todo, deleteTodo, toggleCompleted }: TodoProps) => {
   return (
-    <div
-      className={`flex flex-col p-4 mb-3 w-full rounded-md cursor-pointer bg-gray-100 md:w-1/2 ${
-        todo.completed && 'line-through'
-      } hover:bg-gray-200`}>
+    <div className={`flex flex-col p-4 mb-3 w-full rounded-md bg-gray-100 md:w-1/2 ${todo.completed && 'line-through'} hover:bg-gray-200`}>
       <div className='flex flex-row justify-between'>
-        <p onClick={() => toggleCompleted(todo.id)}>{todo.title}</p>
+        <p onClick={() => toggleCompleted(todo.id)} className='font-semibold hover:text-green-500 cursor-pointer w-full'>
+          {todo.title}
+        </p>
 
-        <GiCancel className='text-black z-10 hover:text-red-500' size='1rem' onClick={() => deleteTodo(todo.id)} />
+        <GiCancel className='text-black z-10 hover:text-red-500' size='1.5rem' onClick={() => deleteTodo(todo.id)} />
       </div>
     </div>
   );
